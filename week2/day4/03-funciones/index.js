f2e24997c17bef4fd0eafed8978c12950261ebf4
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-09-29 15:10:41 +0200

    argumentos en funciones

---
+++ week2/day4/03-funciones/index.js
@@ -0,0 +1,18 @@
+// Si son mas de 3 argumentos no seria recomendado asi
+const funcion = (arg1, arg2, arg3, arg4, arg5)=>{
+
+}
+// Si son mas 3 argumentos mejor pasar un objeto
+const function2 = ({arg1, arg2, arg3, arg4}) => {
+
+}
+
+funcion(1, 2, 3, 4);
+const obj ={
+    arg1: '',
+    arg2: '',
+    arg3: '',
+    arg4: '',
+    arg5: ''
+}
+function2(obj)
