bb8c57a7c123fe62d82f4d3d02b9a5629972a5d7
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-09-30 14:07:51 +0200

    Update index.js

--- week2/day5/02-reto/index.js
+++ week2/day5/02-reto/index.js
@@ -4,6 +4,65 @@
 // Reto 4 - Hacer una array con la informacion de los 151 primeros pokemons
 // Reto 5 - Mostrar en la lista el nombre y la imagen de los pokemons
 
+
+// Paso 1
+const paso1 = async () => {
+    // Fetch retorna una promesa con una respuesta
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+    // json retorna una promesa con el contenido del fetch
+    const data = await response.json();
+    console.log(data.name)
+}
+
+// Paso 2
+// Hace el html con un input
+
+// Paso 3
+const paso3 = () => {
+    const input = document.querySelector('#search');
+    input.addEventListener('keyup', () => {
+        console.log(input.value);
+    })
+}
+
+// Paso4
+const paso4A = async () => {
+    // OPCION A (LA QUE HAY QUE ENTENDER)
+    // Creamos la array donde vamos a guardar la informacion de los pokemons
+    const pokemons = [];
+    // Buscamos los pokemon con los id del 1 al 151
+    for (let id = 1; id <= 151; id++) {
+        // fetch nos da le respuesta de la peticion
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        // con el json obtenemos la informacion de la respuesta
+        const data = await response.json();
+        // Guardamos la informacion del pokemon en la array que hemos creado
+        pokemons.push(data)
+    }
+    // En la variable pokemons tendremos la informacion de todos los pokemons que hemos consultado
+}
+
+const paso4B = async () => {
+    const fetches = [];
+
+    for (let id = 1; id <= 151; id++) {
+        const fetchs = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        fetches.push(fetchs)
+    }
+    // Despues del for tendremos una array con las promesas de los pokemon
+    const data = await Promise.all(fetches);
+    const responses = [];
+    // Recorremos la array con las respuestas de la promesa del fetch
+    for (let i = 0; i < data.length; i++) {
+        // En element tenemos el resultado de la promesa, linea 13 del paso1
+        const element = array[i];
+        const promesaJson = element.json();
+        responses.push(promesaJson);
+    }
+    // Tendremos en respones una array con las promesas del json
+    const pokemons = await Promise.all(responses);
+}
+
 // Opcion A - hacemos una peticion despues de que termine la anterior
 const getData = async () => {
     const pokemons = [];
@@ -13,11 +72,6 @@ const getData = async () => {
         pokemons.push(data)
     }
     console.log(pokemons);
-
-
-
-
-
 }
 
 // Hacemos una peticion a la pokeapi
@@ -31,7 +85,7 @@ const getAllPokemons = async (quantity) => {
     const promises = [];
     // Guardamos en una array la promesa del fetch
     for (let i = 1; i <= quantity; i++) {
-       promises.push(pokeApi(i))
+        promises.push(pokeApi(i))
     }
     // console.log(promises)
     // Esperamos a que todas las promesas de fetch terminen
@@ -49,22 +103,22 @@ const getAllPokemons = async (quantity) => {
 }
 
 const search = document.querySelector('#search');
-search.addEventListener('keyup', ()=>{
-   console.log(search.value) 
-} )
+search.addEventListener('keyup', () => {
+    console.log(search.value)
+})
 
 // No podemos hacer un await fuera de una funcion async
 // const pokemon = await  getAllPokemons(151)
 
 // OPCION - A 
-const main = async () =>{
+const main = async () => {
 
 }
 main()
 // OPCION - B
 // Este evento se ejecutara cuando inicie la pagina
 window.addEventListener('load', async () => {
-   
+
     // OPCION A
     // const pokemons = [];
     // for (let i = 1; i <= 151; i++) {
@@ -75,7 +129,6 @@ window.addEventListener('load', async () => {
     // OPCION - B
     const pokemons = await getAllPokemons(151)
     console.log(pokemons);
-
 })
 
 
