93d8cb3061be1c69435c01c2e75255f1036978a6
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-10-17 13:35:53 +0200

    html

--- week5/day1/03-login/src/views/Login.vue
+++ week5/day1/03-login/src/views/Login.vue
@@ -1,5 +1,33 @@
 <template>
-    <h1>LOGIN</h1>
+    <div class="section">
+        <div class="container">
+            <form>
+                <div class="field">
+                    <label class="label">Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Nombre">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                        <input class="input" type="email" placeholder="email">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                        <input class="input" type="password" placeholder="password">
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <input class="button is-link is-fullwidth" type="submit" placeholder="Text input">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     <!-- 
     1 - Hacer un formulario de login con los inputs de 
     nombre, email y contrasena 
@@ -10,4 +38,5 @@
 <script setup>
 </script>
 <style scoped>
+
 </style>
